df9d30ab547abe871a71a0b434974d88f7613a43
Brito, Tailane <tailane.brito01@gmail.com>
2020-12-05 15:13:09 -0300

    [MATSU-App] adde pages sobrenos contato agendamento to the application and set links

--- api/routes/index.js
+++ api/routes/index.js
@@ -7,8 +7,18 @@ router.get('/', (req, res) => {
     res.render('index.html');
 });
 
+router.get('/agendamento', (req, res) => {
+    res.render('pages/agendamento.html')
+});
+
+
+
 router.get('/contato', (req, res) => {
-    res.render('contato.html')
+    res.render('pages/contato.html')
+});
+
+router.get('/sobrenos', (req, res) => {
+    res.render('pages/sobrenos.html')
 });
 
 module.exports = router;
